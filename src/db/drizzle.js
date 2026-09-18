2d18a01e55509c40ec26072e71902c61ca76daef
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2026-09-18 02:11:29 -0300

    log db connection errors

--- src/db/drizzle.js
+++ src/db/drizzle.js
@@ -1,6 +1,8 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+import logger from '../utils/logger';
+
 const {
   DB_NAME,
   DB_USERNAME,
@@ -10,6 +12,13 @@ const {
 
 const pool = new Pool({
   connectionString: `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
+});
+
+pool.on('error', (err) => {
+  logger.error({
+    message: 'Caught unexpected PostgreSQL pool error on drizzle.js',
+    error: err,
+  });
 });
 
 const db = drizzle(pool);
